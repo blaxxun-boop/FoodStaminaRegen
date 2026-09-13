3fd50152e700bb43e766b475f6cf537c21376b18
Blaxxun <brabbelchen@hotmail.de>
2026-09-13 02:39:05 +0200

    fixes for 1.0

--- FoodStaminaRegen/FoodStaminaRegen.cs
+++ FoodStaminaRegen/FoodStaminaRegen.cs
@@ -1,4 +1,5 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
 using System.Collections.Generic;
@@ -14,7 +15,7 @@ namespace FoodStaminaRegen;
 public class FoodStaminaRegen : BaseUnityPlugin
 {
 	private const string ModName = "Stamina Regeneration from Food";
-	private const string ModVersion = "1.5.7";
+	private const string ModVersion = "1.5.8";
 	private const string ModGUID = "org.bepinex.plugins.foodstaminaregen";
 
 	private static readonly ConfigSync configSync = new(ModName) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
@@ -26,7 +27,6 @@ public class FoodStaminaRegen : BaseUnityPlugin
 	}
 
 	private static ConfigEntry<Toggle> serverConfigLocked = null!;
-	private static ConfigEntry<Toggle> isEnabled = null!;
 	private static ConfigEntry<float> regMultiplier = null!;
 
 	private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
@@ -45,7 +45,6 @@ public class FoodStaminaRegen : BaseUnityPlugin
 		
 		serverConfigLocked = config("1 - General", "Config is locked", Toggle.On, new ConfigDescription("If on, only admins can change the configuration on a server."));
 		configSync.AddLockingConfigEntry(serverConfigLocked);
-		isEnabled = config("1 - General", "Enabled", Toggle.On, new ConfigDescription("If the mod is enabled."));
 		regMultiplier = config("1 - General", "Multiplier for all foods", 1f, new ConfigDescription("Can be used to multiply all base regeneration values configured below."));
 
 		mod = this;
@@ -68,9 +67,9 @@ public class FoodStaminaRegen : BaseUnityPlugin
 
 			Regex regex = new(@"[=\n\t\\""'\[\]]*");
 
-			List<ItemDrop.ItemData.SharedData> items = ObjectDB.instance.m_items.Select(i => i.GetComponent<ItemDrop>().m_itemData.m_shared).ToList();
+			List<ItemDrop.ItemData.SharedData> items = ObjectDB.instance.m_items.Where(i => i.GetComponent<ItemDrop>()).Select(i => i.GetComponent<ItemDrop>().m_itemData.m_shared).ToList();
 			staminaRegen = items.Where(item => item.m_itemType == ItemDrop.ItemData.ItemType.Consumable && item.m_foodStamina > 0)
-				.ToDictionary(item => item.m_name, item => mod.config("2 - Food", regex.Replace(english.Localize(item.m_name), ""), item.m_foodStamina * 0.03f, new ConfigDescription("", null, new ConfigurationManagerAttributes { DispName = regex.Replace(Localization.instance.Localize(item.m_name), "") })));
+				.ToDictionary(item => item.m_name, item => mod.config("2 - Food", regex.Replace(english.Localize(item.m_name), ""), (float)Math.Round(item.m_foodStamina * 0.03f, 2), new ConfigDescription("", null, new ConfigurationManagerAttributes { DispName = regex.Replace(Localization.instance.Localize(item.m_name), "") })));
 		}
 	}
 
@@ -81,31 +80,28 @@ public class FoodStaminaRegen : BaseUnityPlugin
 
 		private static void Prefix(Player __instance)
 		{
-			if (isEnabled.Value == Toggle.On)
+			basisStaminaRegen ??= __instance.m_staminaRegen;
+
+			float totalStaminaRegen = (float)basisStaminaRegen;
+
+			foreach (Player.Food food in __instance.m_foods)
 			{
-				basisStaminaRegen ??= __instance.m_staminaRegen;
-
-				float totalStaminaRegen = (float)basisStaminaRegen;
-
-				foreach (Player.Food food in __instance.m_foods)
+				if (staminaRegen.TryGetValue(food.m_item.m_shared.m_name, out ConfigEntry<float> regen))
 				{
-					if (staminaRegen.TryGetValue(food.m_item.m_shared.m_name, out ConfigEntry<float> regen))
-					{
-						totalStaminaRegen += regen.Value * regMultiplier.Value;
-					}
+					totalStaminaRegen += regen.Value * regMultiplier.Value;
 				}
-
-				__instance.m_staminaRegen = totalStaminaRegen;
 			}
+
+			__instance.m_staminaRegen = totalStaminaRegen;
 		}
 	}
 
-	[HarmonyPatch(typeof(ItemDrop.ItemData), nameof(ItemDrop.ItemData.GetTooltip), typeof(ItemDrop.ItemData), typeof(int), typeof(bool), typeof(float), typeof(int))]
+	[HarmonyPatch(typeof(ItemDrop.ItemData), nameof(ItemDrop.ItemData.GetTooltip), typeof(ItemDrop.ItemData), typeof(int), typeof(bool), typeof(float), typeof(int), typeof(bool))]
 	private class FoodDescPatch
 	{
 		private static void Postfix(ItemDrop.ItemData item, ref string __result)
 		{
-			if (isEnabled.Value == Toggle.On && staminaRegen.TryGetValue(item.m_shared.m_name, out ConfigEntry<float> regen))
+			if (staminaRegen.TryGetValue(item.m_shared.m_name, out ConfigEntry<float> regen))
 			{
 				__result += "\n$fsr_stat_name: <color=orange>" + regen.Value * regMultiplier.Value * 10 + "%</color>";
 			}
